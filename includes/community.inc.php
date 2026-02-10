2976de42d358a711cf889c1a9ec06de2b137c9e8
Mikhail Stupalov <landy2005@gmail.com>
2026-02-10 10:55:36 +0100

    Fix community.inc.php (#9)

--- includes/community.inc.php
+++ includes/community.inc.php
@@ -1,9 +1,11 @@
 <?php
 
-// stub functions for not exist features 
-function cache_groups() {return [];}
-function get_type_groups($type = null, $reset = true) {return [];}
-function get_groups_by_type($type = null) {return [];}
-function get_group_entities($group, $entity_type = '') {return [];}
-function get_group_entities_array($group, $entity_type = '') {return [];}
+// stub functions for not exist features
+function cache_groups() { return []; }
+function get_type_groups($type = NULL, $check_permission = TRUE) { return []; }
+function get_groups_by_type($type = NULL) { return []; }
+function get_group_entities($group_ids, $entity_type = '') { return []; }
+function get_group_entities_array($group_ids, $entity_type = '') { return []; }
+function get_entity_group_names($entity_type, $entity_id) { return []; }
+
 // EOF
