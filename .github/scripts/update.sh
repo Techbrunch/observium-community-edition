e73f0af9ed24d1e25eae41a618dd9580a70d7dc4
Daniëlle Huisman <danielle@huisman.me>
2026-03-21 10:43:18 +0100

    Add community patch to update script (#12)

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -17,4 +17,20 @@ rsync -ra --exclude logs --exclude rrd --exclude .new --exclude .git* --delete-a
 echo "Deleting archive..."
 rm -rf .new
 
+# Taken from https://www.observium.org/observium_installscript.sh
+echo "Patching includes/community.inc.php with stub functions"
+cat > "includes/community.inc.php" << 'CMNT'
+<?php
+
+// stub functions for not exist features
+function cache_groups() { return []; }
+function get_type_groups($type = NULL, $check_permission = TRUE) { return []; }
+function get_groups_by_type($type = NULL) { return []; }
+function get_group_entities($group_ids, $entity_type = '') { return []; }
+function get_group_entities_array($group_ids, $entity_type = '') { return []; }
+function get_entity_group_names($entity_type, $entity_id) { return []; }
+
+// EOF
+CMNT
+
 echo "Update complete."
